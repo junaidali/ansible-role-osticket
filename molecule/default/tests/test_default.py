3197620ab642027b7dcd6e5df9caf8ce51454281
Syed Junaid Ali <syedjunaid.ali@slalom.com>
2019-04-25 16:13:01 -0500

    testing for web server listening.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_web_server_running(host):
+    s = host.socket("tcp://0.0.0.0:80")
+    assert s.is_listening
